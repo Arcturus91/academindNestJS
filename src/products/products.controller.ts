f36a24207d2453672dc3d30be017c654eaa1a93b
Arcturus91 <arturo.barrantesv@pucp.pe>
2022-12-21 21:03:04 -0500

    getProducts and getSingleProduct 100%

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,16 +1,16 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
-  constructor(private readonly productService: ProductsService) {
+  constructor(private readonly productsService: ProductsService) {
     //you are using the type as a class. Also, we are adding the access modifier "private" for making the property directly available without having to declare it.
   }
   @Post()
   addProduct(
     @Body() requestBody: { title: string; description: string; price: number },
   ) {
-    const generatedId = this.productService.insertProduct(
+    const generatedId = this.productsService.insertProduct(
       requestBody.title,
       requestBody.description,
       requestBody.price,
@@ -18,7 +18,15 @@ export class ProductsController {
     return { id: generatedId }; //because we are returning an objet, NestJS will set the content-type header as a JSON.
   }
 
+  @Get()
+  getAllProducts() {
+    return this.productsService.getProducts();
+  }
+  //in the controller is where i build the app functionality
 
-@Get
-  getAllProducts(){}
+  @Get(':id') //if i only put the Get decorator, it will not work. the first get will take over.
+  //thats why we add a parameter
+  getProduct(@Param('id') prodId: string) {
+    return this.productsService.getSingleProduct(prodId);
+  }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -5,7 +5,7 @@ import { Product } from './product.model';
 @Injectable()
 export class ProductsService {
   private products: Product[] = []; //estamos diciendo que queremos un array de elementos tipo Product (la clase modelo que creamos en product.model.ts)
-// le agregamos el private para que no pueda ser accedido desde otro lugar que esta clase.
+  // le agregamos el private para que no pueda ser accedido desde otro lugar que esta clase.
   insertProduct(title: string, product: string, price: number) {
     const id = uuidv4();
     const newProduct = new Product(id, title, product, price);
@@ -13,10 +13,20 @@ export class ProductsService {
     return String(id);
   }
 
+  getProducts() {
+    //return this.products; //object and arrays are reference types in JS / This will return a pointer to the same list memory. Not good idea to return something that is outside the service. Better woould be to create a copy so we can handle it internally from the service
+    return [...this.products]; // this way creates a copy of the original list content (the objects) and returns it.
+  }
 
-
-
-
-
-
+  getSingleProduct(id: string) {
+    const productsList = [...this.products];
+    return productsList.find((p) => p.id === id);
+  }
 }
+
+/*
+Objects, arrays, and functions are reference types.
+A primitive type has a fixed size in memory. // the largest of the primitive types.
+Number occupies eight bytes of memory
+Boolean value can be represented with only one bit. 
+*/
